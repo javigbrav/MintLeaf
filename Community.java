f8a473274d6003f2713d17008b44b90cbc4ce118
FhebReker <146902117+FhebReker@users.noreply.github.com>
2023-12-20 13:50:22 -0500

    Create Community.java

---
+++ Community.java
@@ -0,0 +1,36 @@
+public class Community{
+  private double rating = 0;
+  public Community(){
+    this.rating = 0;
+  }
+
+  public void recommendationQuiz() {
+        
+    }
+
+    public void rateStory() {
+        
+    }
+
+    public double averageRating() {
+        
+        return rating;
+    }
+
+    public void adjustRankings() {
+        
+    }
+
+    public void reportStory() {
+        
+    }
+
+    public void suggestStory() {
+        
+    }
+
+    public boolean isApproved() {
+        
+        return approved;
+    }
+}
