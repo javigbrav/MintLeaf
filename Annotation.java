371ba52d700cdd2807c860e727b0f5052f0257b3
FhebReker <146902117+FhebReker@users.noreply.github.com>
2024-01-02 19:45:59 -0500

    Add files via upload

---
+++ Annotation.java
@@ -0,0 +1,30 @@
+package StoryInteraction;
+
+import java.awt.Color;
+
+public class Annotation{	
+	public String note;
+	public Color highlightColour;
+	
+	
+	public Story getStory() {
+		// add stuff here
+		return null;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void addNote() {
+		// add stuff here
+	}
+	public void removeNote() {
+		// add stuff here
+	}
+	public Color pickColour() {
+		// add stuff here
+		return null;
+	}
+	public void highlight(String colour, int startIndex, int endIndex) {
+		// add stuff here
+	}
+}
