6f4484d736c3736a560fbdacd00c1419c3221dac
Zainab-06 <154269872+Zainab-06@users.noreply.github.com>
2024-01-16 13:58:27 -0500

    Update Tales.java

--- Tales.java
+++ Tales.java
@@ -1,11 +1,16 @@
+package mains;
 import java.awt.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.*;
 import javax.swing.*;
+import user.*;
+import java.sql.*;
 /***********************************************************************************
  * Author: Zainab Siddiqui 
  * Date: December 20, 2023 
- * Last Modified: January 14, 2024
- * Last Modified by: Fardin Abbassi
+ * Last Modified: 15 Jan
+ * Last Modified by: 
  * Description: the main/driver for the application
  ***********************************************************************************/
 public class Tales {
@@ -14,18 +19,41 @@ public class Tales {
 	static Rectangle rect = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().getBounds();
 	public static int screenW = rect.width; //sets the screen width
     public static int screenH = rect.height; //sets the screen height
-    
     private static LinkedList<User> users;
-    static String username; 
-    private String password;
-    
+    public static String username; public String password;
+    Connection con;
     /*Constructor*/
     public Tales(LinkedList<User> users) {
+    	createConnection();
     	this.users = users;
+		User usernameAndPassword = new User(username, password);
 		LoginPage loginpage = new LoginPage();
 	}
-	
-	public static void main(String[] args) {
+
+    /*Create connection to MySQL*/
+    void createConnection() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); // connecting MySQL driver to program
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mintleaf_db", "root", "root");
+		} catch (ClassNotFoundException e) {
+			Logger.getLogger(Tales.class.getName()).log(Level.SEVERE, null, e);
+		} catch (SQLException e) {
+			Logger.getLogger(Tales.class.getName()).log(Level.SEVERE, null, e);
+		}	
+	} // end createConnection
+
+    public static void setFrame(JFrame frame) {
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	frame.setSize(Tales.screenW, Tales.screenH);
+    	frame.getContentPane().setLayout(null);
+    	frame.getContentPane().setBackground(new Color(220, 242, 206));
+    	frame.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\kashi\\Downloads\\mintleaf_icon.png")); // MintLeaf Logic logo without text
+    	frame.setBackground(new Color(220, 242, 206));
+    	frame.setForeground(Color.black);
+    }
+    
+    public static void main(String[] args) {
 		new Tales(users);
 	} // end main
+
 } // end Tales
