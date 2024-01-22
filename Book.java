f82c323952d48a0b1188ab5dcda0c018a92fe2dc
FhebReker <146902117+FhebReker@users.noreply.github.com>
2024-01-21 19:23:51 -0500

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,4 +1,4 @@
-package LogIn;
+package StoryInteraction;
 
 import javax.swing.*;
 import javax.swing.text.SimpleAttributeSet;
@@ -8,6 +8,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import Homepage.*;
+import Community.*;
 
 public class Book extends JFrame{
     private JTextPane textPane;
@@ -20,15 +22,15 @@ public class Book extends JFrame{
 	public String country;
 	public String language;
 	public float rating; // making sense with the table in the database
-	
+	private double userRating = 0;
+
 	public String genre;
 	public String region;
 	public String ageGroup; // change from UML: String instead of int
 	public String storyLength;
 	private static int priority = 1;
 	
-	Object[] options = {"Rate",
-    "Open"};
+	Object[] options = {"Rate", "Open"};
 	
 	public int votes;
 //    Book (String bookName){
@@ -45,7 +47,7 @@ public class Book extends JFrame{
     public void setLanguage (String language) {this.language = language;}
     public void setRating (float rating) {this.rating = rating;}
     
-	Book (String bookName){
+	public Book (String bookName){
 //		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //		setSize (250, 175);
 //		setLocationRelativeTo(book);
@@ -70,8 +72,9 @@ public class Book extends JFrame{
 					    null,     //do not use a custom Icon
 					    options,  //the titles of buttons
 					    options[0]); //default button title
-				if (result == JOptionPane.YES_OPTION);
-	                //rate ();
+				if (result == JOptionPane.YES_OPTION) {
+					rateBook();
+				}
 	            else if (result == JOptionPane.NO_OPTION)
 	                new StoryInteraction (bookName);
 			}
@@ -79,6 +82,62 @@ public class Book extends JFrame{
 	}
 	static void increasePriority() {priority++;}
 	public int getPriority() {return priority;}
+	
+	/* Method Name: rateBook 
+	 * Author: Javiera Garrido Bravo
+	 * Creation Date: January ??, 2024
+	 * Modified Date: January ??, 2024
+	 * Description: creates a frame for the user to rate the given book(?)
+	 * @Parameters: n/a
+	 * @Return Value: void
+	 * Data Type: n/a
+	 * Dependencies: ?????
+	 * Throws/Exceptions: ????
+	 */    
+	private void rateBook() {
+		// sets up components
+		JFrame rateFrame = new JFrame();
+		rateFrame.setLayout(new FlowLayout());
+		JLabel desc = new JLabel("Rate this story out of 5:");
+		JCheckBox[] ratings = new JCheckBox[5];
+        ButtonGroup ratingGroup = new ButtonGroup();
+        JButton submitButton = new JButton("Confirm rating");
+        
+        // adds everything to frame
+        rateFrame.add(desc);
+        for (int i = 0; i < 5; i++) {
+        	ratings[i] = new JCheckBox(String.valueOf(i+1));
+        	ratingGroup.add(ratings[i]);
+            rateFrame.add(ratings[i]);
+            ratings[i].setFont(new Font("Calibri", Font.PLAIN, 15));
+        }
+        rateFrame.add(submitButton);
+        submitButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        rateFrame.setVisible(true);
+        rateFrame.setPreferredSize(new Dimension(25, 4)); 
+        rateFrame.setSize(500,75);
+        rateFrame.setLocationRelativeTo(book);
+        
+        // submit button listener
+        submitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	// looks through each checkbox to find user rating
+                for (int i = 0; i < ratings.length; i++) {
+                    if (ratings[i].isSelected()) {
+                        userRating = Double.parseDouble(ratings[i].getText());
+                        break;
+                    }
+                }
+
+                // if user adds a rating, dispose frame, add user rating to database
+                if (userRating != 0) {
+                    rateFrame.dispose();
+                    Community.rateStory(userRating, title);;
+                }
+            }
+        });
+	}
+	
 	
 	public void showThis() {
 		
