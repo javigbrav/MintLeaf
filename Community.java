7f510807ec0946060080a168cf37cfac8e14a80d
FhebReker <146902117+FhebReker@users.noreply.github.com>
2024-01-21 21:07:42 -0500

    Update Community.java

--- Community.java
+++ Community.java
@@ -3,7 +3,7 @@ package Community;
 /***********************************************************************************
  * Author: Fardin Abbassi
  * Date: December 20, 2023 
- * Last Modified: January 18, 2024
+ * Last Modified: January 21, 2024
  * Last Modified by: Fardin Abbassi
  * Description: A class full of methods for user/system interactions
  ***********************************************************************************/
@@ -11,7 +11,7 @@ package Community;
 import java.sql.*;
 import java.util.LinkedList;
 import Homepage.*;
-import StoryInteraction.Story;
+import StoryInteraction.Book;
 
 public class Community{
 	private double rating = 0;
@@ -22,23 +22,14 @@ public class Community{
 
 	/**A TreeNode object represents a single book node in a binary(?) tree. **/
 	private static class TreeNode {
-		Story book; 			// The book.
-		String genre;      		// The book's genre.
-		String region;    		// The book's region.
-		String ageRange;     	// The book's age range.
-		String length;     		// The book's length.
-		int priority;			// The book's relevance to the user.
+		Book book; 			// The book.
+		int priority;		// The book's relevance to the user.
 		
 		TreeNode left;    // Pointer to left subtree.
 		TreeNode right;   // Pointer to right subtree.
-		TreeNode(Story book, String genre, String region, String ageRange, String length, int priority) { // Constructor.
-			// Make a node containing the specified string.
-			// Note that left and right pointers are initially null.
+		// 
+		TreeNode(Book book, int priority) { // Constructor.
 			this.book = book;
-			this.genre = genre;
-			this.region = region;
-			this.ageRange = ageRange;
-			this.length = length;
 			this.priority = priority;
 		}
 	}  // end TreeNode class
@@ -48,21 +39,21 @@ public class Community{
 	/* Method Name: tailoredReccomendations 
 	 * Author: Fardin Abbassi 
 	 * Creation Date: January 09, 2024
-	 * Modified Date: January ??, 2024
+	 * Modified Date: January 21, 2024
 	 * Description: Makes a binary tree based on the user's answers to questions from the recommendation quiz, 
-	 * 				increase story priority based on matches to user preference, ADD OTHER THINGS HERE.
-	 * @Parameters: LinkedList<Story> booksToSort, User user
-	 * @Return Value: LinkedList<Story>
+	 * 				increase story priority based on matches to user preference, then sends the tailored list back.
+	 * @Parameters: LinkedList<Book> booksToSort, User user
+	 * @Return Value: LinkedList<Book>
 	 * Data Type: ???
 	 * Dependencies: ?????
 	 * Throws/Exceptions: ????
 	 */    
-	public static LinkedList<Story> tailoredRecomendations(LinkedList<Story> booksToSort, User user) {
+	public static LinkedList<Book> tailoredRecomendations(LinkedList<Book> booksToSort, User user) {
 		// add each story in the linked list into a binary tree
-  		for (Story story : booksToSort) {
+  		for (Book book : booksToSort) {
   			// if root is null, make the root the only node in the tree
   			if(root == null) {
-  				root = new TreeNode(story, story.genre, story.region, story.ageGroup, story.storyLength, story.getPriority());
+  				root = new TreeNode(book, book.getPriority());
   			}
 		  
   			// set runner to root's value
@@ -75,26 +66,38 @@ public class Community{
   			while(!finishedChecks) {
   				switch(preferenceToCheck) {
   					case 0: // comparing to user's genre preference
-  						if(story.genre == user.genre)
-  							story.increasePriority();
+  						if(book.genre == user.genre)
+  							book.increasePriority();
   						break;
 					  
   					case 1: // comparing to user's region preference
-  						if(story.region == user.region)
-  							story.increasePriority();
+  						if(book.region == user.region)
+  							book.increasePriority();
   						break;
 					  
   					case 2: // comparing to user's age
   						/** For future reference, each story's age group should be something like "00-13" **/
-  						int age1 = Integer.parseInt(story.ageGroup.substring(0,1));
-  						int age2 = Integer.parseInt(story.ageGroup.substring(2));
-  						if(age1 < user.age && user.age > age2)
-  							story.increasePriority();
+  						String ages = book.ageGroup;
+  						int age1 = 0;
+  						int age2 = 0;
+  						switch (ages) {
+	  						case "Child":
+	  							age1 = 0;
+	  							age2 = 15;
+	  						case "Young Adult":
+	  							age1 = 15;
+	  							age2 = 25;
+	  						case "Adult":
+	  							age1 = 25;
+	  							age2 = 100;
+  						}
+  						if(age1 <= user.age && user.age > age2)
+  							book.increasePriority();
   						break;
 					  
   					case 3: // comparing to user's story length preference
-  						if(story.storyLength == user.storyLength)
-  							story.increasePriority();
+  						if(book.storyLength == user.storyLength)
+  							book.increasePriority();
   						break;
 					  
   					case 4: // finish priority checks
@@ -107,19 +110,19 @@ public class Community{
   			// add the story to the binary tree based on priority found above
   			while(true) {
   				// If the story is more relevant to the user than the runner, bring it to the left subtree of the runner
-  				if(story.getPriority() >= runner.priority) {
+  				if(book.getPriority() >= runner.priority) {
   					if(runner.left == null) {
-  						runner.left = new TreeNode(story, story.genre, story.region, story.ageGroup, story.storyLength, story.getPriority());
+  						runner.left = new TreeNode(book, book.getPriority());
   						break;
   					}
   					// If the left node of the runner is not null, set runner to left node
   					else runner = runner.left;
   				} // end if
 			  
-  				// If the story is less relevant to the user than the runner, bring it to the right subtree of the runner
+  				// If the book is less relevant to the user than the runner, bring it to the right subtree of the runner
   				else {
   					if (runner.right == null) {
-  						runner.right = new TreeNode(story, story.genre, story.region, story.ageGroup, story.storyLength, story.getPriority());
+  						runner.right = new TreeNode(book, book.getPriority());
   						break;
   					}
   					// If the right node of the runner is not null, set runner to right node
@@ -129,7 +132,7 @@ public class Community{
   		} // end for loop
   		
   		// sorts the binary tree of books to accomodate the user
-  		LinkedList<Story> recommendedBooks = new LinkedList<Story>();
+  		LinkedList<Book> recommendedBooks = new LinkedList<Book>();
   		recommendedList(root, recommendedBooks);
   		return recommendedBooks;
     }
@@ -138,13 +141,13 @@ public class Community{
 	 * Creation Date: January 12, 2024
 	 * Modified Date: January 12, 2024
 	 * Description: Goes inorder through a binary tree, and sorts books into the given linked list of stories
-	 * @Parameters: TreeNode node, LinkedList<Story> stories
+	 * @Parameters: TreeNode node, LinkedList<Book> stories
 	 * @Return Value: void
 	 * Data Type: n/a
 	 * Dependencies: ?????
 	 * Throws/Exceptions: ????
 	 */ 
-	private static void recommendedList(TreeNode node, LinkedList<Story> stories) {
+	private static void recommendedList(TreeNode node, LinkedList<Book> stories) {
 		if(node != null) {
 			recommendedList(node.left, stories);
 			stories.add(node.book);
@@ -153,8 +156,8 @@ public class Community{
 	}
 
   	
-  	/**MAIN IDEA: Preface this by adding individual story pages, then add stars that act as buttons that lets a user do this.
-  				  Depending on input rating, change user rating of the story to match. (Basic idea as of now)**/
+  	/**MAIN IDEA: Preface this by adding individual book pages, then add stars that act as buttons that lets a user do this.
+  				  Depending on input rating, change user rating of the book to match. (Basic idea as of now)**/
 	/** Might take this idea and move it into the star buttons themselves if/when they become available **/
   	public static void rateStory(double userRating, String title) {
 		try {
@@ -180,7 +183,7 @@ public class Community{
     }
 
   	
-    /**MAIN IDEA: 	Preface this by having a story page for each story.
+    /**MAIN IDEA: 	Preface this by having a book page for each story.
     				Have a button that allows a user to add a report to a table in the database for reports. 
     				Reports should track which user created the report, which story the report is in, and the contents of the report.
     				(Basic idea as of now, could possibly just be a part of the story page's class instead)**/
@@ -224,10 +227,13 @@ public class Community{
         return approved;
     }								*/
 
-
-	/** TEST AREA **/
+ 	
+ 	
+ 	/** TEST AREA **/
  	public static void main(String[] args) {
  		Community.rateStory(4.5, "Ramayana");
  		Community.reportStory("test", "Aladdin", "Improper translation at line 8");
  	}
+ 	
+ 	
 }
