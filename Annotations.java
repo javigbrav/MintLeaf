22e9e21cabb259486413561b22c745b312d8555b
javigbrav <154269308+javigbrav@users.noreply.github.com>
2024-01-22 19:51:42 -0500

    Update Annotations.java
    comments

--- Annotations.java
+++ Annotations.java
@@ -1,3 +1,9 @@
+/***********************************************************************************
+ * Author: Javiera Garrido
+ * Date: December 20, 2023 
+ * Last Modified: 22 Jan, 2024
+ * Description: creates a frame to interact with the annotations
+ ***********************************************************************************/
 
 import javax.swing.*;
 
@@ -8,49 +14,39 @@ import java.awt.event.ActionListener;
 import java.io.*;
 
 public class Annotations extends JFrame {
-    private static JTextArea textArea;
-    private JButton buttonSave;
-    private JButton buttonSee;
-    private static String username;
-    private static String story;
-//    private static Connection con;
-//    private PreparedStatement preparedStatement;
-    
-    public static void setUsername (String username1) {username = username1;}
-    public static void setStory(String story1) {story = story1;}
-    public static String getUsername () {return username;}
-    public static String getStory() {return story;}
+    private static JTextArea textArea; //tex area to write in
+    private JButton buttonSave;// save button
+    private JButton buttonSee; //button to see annotations
+    private static String username; //username
+    private static String story;// name of the story
+	
+    public static void setUsername (String username1) {username = username1;} //set username of the user reading the book
+    public static void setStory(String story1) {story = story1;} //set name of the story
     
     public Annotations() {
         super("Annotation");
         initComponents();
-//        try {
-//            con = DriverManager.getConnection("jdbc:mysql://mintleafdb", "root", "root");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            // Handle connection failure
-//        }
     }
 
     private void initComponents() {
         textArea = new JTextArea(10, 40);
-        JScrollPane scrollPane = new JScrollPane(textArea);
+        JScrollPane scrollPane = new JScrollPane(textArea); //initialize a scroll bar in case annotations exceed the size of the frame
         
-        buttonSave = new JButton ("Save Annotation"); 
-        buttonSee = new JButton ("See Annotation History");
+        buttonSave = new JButton ("Save Annotation"); //initialize button save
+        buttonSee = new JButton ("See Annotation History"); //initialize button see
         
-        setLayout(new BorderLayout());
-        add(scrollPane, BorderLayout.CENTER);
+        setLayout(new BorderLayout()); //create layout
+        add(scrollPane, BorderLayout.CENTER); //add scrollpane
 
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //dont close everything, only this frame
         setSize(500, 300);
         setLocationRelativeTo(null);
         setVisible(true);
         
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(buttonSave);
-        buttonPanel.add(buttonSee);
-        add (buttonPanel, BorderLayout.SOUTH);
+        buttonPanel.add(buttonSee); //add buttons to panel
+        add (buttonPanel, BorderLayout.SOUTH);//put panel at the bottom
         
         buttonSave.addActionListener(new ActionListener() {
             
@@ -58,43 +54,33 @@ public class Annotations extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				String content = textArea.getText();
-                //addAnnotation (content);
-                //System.out.println("Text: " + content);
-                addAnnotation (username, story, content);
+                		addAnnotation (username, story, content); //adds the annotation to the database
 			}
-        });
+        	});
         buttonSee.addActionListener(new ActionListener() {
-            
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				seeAnnotations();
+				seeAnnotations(); //shows all annotations that the user has made in this book 
 			}
         });
     }
-//    private void saveAnnotation(String annotationText) {
-//        
-//        // SQL query to insert data into the annotations table
-//        String insertQuery = "INSERT INTO annotations (story, username, annotation_text) VALUES ('"+story+"', '" +username+"', '" + annotationText+"')";
-//
-//        try {
-//            preparedStatement = con.prepareStatement(insertQuery);
-//            // Assuming you have a 'storyName' and 'username' variable representing your data
-//            preparedStatement.setString(1, story);
-//            preparedStatement.setString(2, username);
-//            preparedStatement.setString(3, annotationText);
-//            preparedStatement.executeUpdate();
-//            System.out.println("Annotation saved to database!");
-//        } catch (SQLException ex) {
-//            ex.printStackTrace();
-//        }
-//    }
-    
+	/* Method Name: seeAnnotations
+	 * Author: Javiera Garrido Bravo
+	 * Creation Date: January 22, 2024
+	 * Modified Date: January 22, 2024
+	 * Description: allows the user to see all annotations that has made in the past in this story
+	 * @Parameters: n/a
+	 * @Return Value: n/a
+	 * Data Type: void
+	 * Dependencies: mySQL
+	 * Throws/Exceptions: error when connecting to mysql 
+	 */  
     public void seeAnnotations () {
     	
 		try {
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mintleafdb", "root", "root");
-	    	Statement stat = con.createStatement();
+	    		Statement stat = con.createStatement();
 			ResultSet rs = stat.executeQuery("SELECT * FROM annotations WHERE username like '" + username
 					+ "' and story like '" + story + "'");
 			while(rs.next()) {
@@ -107,12 +93,18 @@ public class Annotations extends JFrame {
 		}			
 		
     }
-    
-    
-    public void addAnnotation(String selectedText) {
-        textArea.append("\n[Annotation]: \"" + selectedText + "\"\n");
-        
-    }
+    /* Method Name: addAnnotations
+	 * Author: Javiera Garrido Bravo
+	 * Creation Date: January 20, 2024
+	 * Modified Date: January 22, 2024
+	 * Description: allows the user to add an annotation to a snippet of the story
+	 * @Parameters: String user: user that uses the application, String storyName: name of the story
+  	 * String annotation: value that's going to be saved
+	 * @Return Value: n/a
+	 * Data Type: void
+	 * Dependencies: mySQL
+	 * Throws/Exceptions: error when connecting to mysql 
+	 */  
     public static void addAnnotation(String user, String storyName, String annotation) {
 		try {
 			
@@ -130,9 +122,4 @@ public class Annotations extends JFrame {
 		
     }
 
-//    public static void main(String[] args) {
-//        SwingUtilities.invokeLater(() -> {
-//            new Annotations();
-//        });
-//    }
 }
